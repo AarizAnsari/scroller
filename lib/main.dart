c8ff2bf92f34835792a031877d71dae74c2e1346
AarizAnsari <aaariz60@gmail.com>
2022-01-09 16:57:21 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+
+
+import 'package:better_player/better_player.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+
+    List<String> links = [
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4",
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4"
+    ];
+
+    int index=0;
+
+
+    return Scaffold(
+      body: Container(
+        height:MediaQuery.of(context).size.height ,
+        width:MediaQuery.of(context).size.width,
+        child: GestureDetector(
+          child: BetterPlayer.network(links[index],
+          betterPlayerConfiguration: BetterPlayerConfiguration(
+            aspectRatio: 9/16,
+            looping: false,
+            autoPlay: true,
+            autoDispose: true,
+            handleLifecycle: true,
+            controlsConfiguration: BetterPlayerControlsConfiguration(
+              showControls: false,
+
+            )
+          ),
+          ),
+          onVerticalDragUpdate:(DragUpdateDetails details) {
+            setState(() {
+              index++;
+            });
+          },
+          onVerticalDragDown:(DragDownDetails details){
+            setState(() {
+              index--;
+            });
+          } ,
+        ),
+      ),
+    );
+  }
+}
+
